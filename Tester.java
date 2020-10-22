dd88c956eb16ae9f017e9539091a847f851ed04d
Christopher Tan <ctan20@stuy.edu>
2020-10-22 14:32:31 -0400

    Added and tested largest method

--- Tester.java
+++ Tester.java
@@ -2,6 +2,13 @@ public class Tester {
     public static void main(String[] args) {
         //sum
         int[] array1 = {1,2,3,4};
+        int[] array2 = {10,5,3,12};
+        int[] array3 = {};
         System.out.println(ArrayOps.sum(array1));
+        System.out.println(ArrayOps.sum(array2));
+        System.out.println(ArrayOps.sum(array3));
+        //largest
+        System.out.println(ArrayOps.largest(array1));
+        System.out.println(ArrayOps.largest(array2));
     }
 }
