6b465042614c1f59f8c9101925bd7e0fff44b40b
Christopher Tan <ctan20@stuy.edu>
2020-10-25 15:37:16 -0400

    Added given google classroom test examples

--- Tester.java
+++ Tester.java
@@ -24,5 +24,12 @@ public class Tester {
         System.out.println(ArrayOps.sum(array4));
         System.out.println(ArrayOps.sum(array5));
         System.out.println(ArrayOps.sum(array6));
+        //checkpoint
+        int[][] A = {{1,0,12,-1},{7,-2,2,1},{-5,-2,2,-9}};
+        int[] B = {1,3,5};
+        System.out.println(Arrays.toString(ArrayOps.sumRows(A)));
+        System.out.println(Arrays.toString(ArrayOps.largestInRows(A)));
+        System.out.println(ArrayOps.sum(B));
+        System.out.println(ArrayOps.sum(A));
     }
 }
